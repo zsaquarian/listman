9a652c8fc1e371462904529708c0e2eea8406cce
Nahos <zsaquar1an.42@gmail.com>
2021-10-15 19:17:41 +0530

    wip: updated example .env file and .env types

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -2,5 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     SESSION_SECRET: string;
+    PG_USER: string;
+    PG_PASSWORD: string;
+    DATABASE_URL: string;
   }
 }
