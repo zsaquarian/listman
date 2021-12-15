a95e0100fe2388b21b6df3bd946f9725331bf426
Nahos <zsaquar1an.42@gmail.com>
2021-12-15 20:23:13 +0530

    refactor(redis): take redis url from .env file

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -6,6 +6,7 @@ declare namespace NodeJS {
     PG_USER: string;
     PG_PASSWORD: string;
     DATABASE_URL: string;
+    REDIS_URL: string;
     GOOGLE_CLIENT_ID_WEB: string;
     GOOGLE_CLIENT_ID_ANDROID: string;
     CORS_ORIGIN: string;
--- server/src/redis.ts
+++ server/src/redis.ts
@@ -1,3 +1,3 @@
 import { createNodeRedisClient } from 'handy-redis';
 
-export const redis = createNodeRedisClient();
+export const redis = createNodeRedisClient({ url: process.env.REDIS_URL });
