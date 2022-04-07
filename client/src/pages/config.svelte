5db4ca863f8a951e213a75aa3da484e2053f80f2
Nahos <zsaquar1an.42@gmail.com>
2022-04-07 19:37:04 +0530

    feat: show version number in config page

--- client/src/pages/config.svelte
+++ client/src/pages/config.svelte
@@ -2,9 +2,10 @@
   import Toggle from '@components/Toggle.svelte';
   import { config } from '@store/config';
   import { Moon, Sun } from '@steeze-ui/heroicons';
+  import { version } from '../../package.json';
 </script>
 
-<div class="flex-col text-center text-3xl m-4 bg-primary-100 rounded-lg p-4 bg-opacity-25 shadow-lg">
+<div class="flex-col text-3xl m-4 bg-primary-100 rounded-lg p-4 bg-opacity-25 shadow-lg">
   <div class="flex gap-5 items-center">
     <Toggle
       label="Dark mode"
@@ -17,4 +18,8 @@
       onIconClass="text-white"
     />
   </div>
+
+  <hr class="my-4" />
+
+  <p class="text-xl text-center">Version no: {version}</p>
 </div>
